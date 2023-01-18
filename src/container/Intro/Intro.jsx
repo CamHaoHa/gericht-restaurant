edc45028e7edd2795d0366e7aba1959fe7538ff6
CamHaoHa <camhaovn@gmail.com>
2023-01-18 11:18:38 +1100

    initial commit

---
+++ src/container/Intro/Intro.jsx
@@ -0,0 +1,46 @@
+import { useRef, useState } from 'react';
+
+import './Intro.css';
+import {BsPlayFill, BsPauseFill} from 'react-icons/bs'
+import {meal } from '../../constants'
+import { MdVideoCameraFront } from 'react-icons/md';
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false)
+  const vidRef = useRef();
+  const handleVideo = () => {
+    setPlayVideo((prePlayVideo) => !prePlayVideo)
+
+    if(playVideo) {
+      vidRef.current.pause()
+    }
+
+    else{
+      vidRef.current.play()
+    }
+  }
+  return(
+  <div className='app__video'>
+    <video 
+    src={meal}
+    ref={vidRef}
+    type='video/mp4'
+    loop
+    controls={false}
+    muted
+    />
+
+  <div className='app__video-overlay flex__center'>
+    <div className='app__video-overlay_circle flex__center'
+          onClick={handleVideo}
+        >
+        {playVideo 
+        ?(<BsPauseFill color='fff' fontSize={30}/>)
+        :(<BsPlayFill color='fff' fontSize={30}/>)
+        }
+    </div>
+  </div>
+
+  </div>
+)};
+
+export default Intro;
